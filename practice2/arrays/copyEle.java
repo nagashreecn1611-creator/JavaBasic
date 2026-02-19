c196fab8fdbdbd2a6cdc7304e6d783e597c44e15
Nagashree CN <nagashreecn1611@gmail.com>
2026-02-19 18:30:30 +0530

    Add files via upload

---
+++ practice2/arrays/copyEle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class copyEle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the Number of Elements :");
+    int n = sc.nextInt();
+
+    int arr1[] = new int [n];
+    int arr2[] = new int [n];
+
+    System.out.println("Enter" +n+ "Elements");
+    for(int i=0;i<n;i++){
+       arr1[i] = sc.nextInt();
+    }
+
+    for(int i=0;i<n;i++){
+      arr2[i] = arr1[i];
+    }
+
+    System.out.println("The Second Array is ");
+    for(int i=0;i<n;i++){
+      System.out.print(arr2[i]+" ");
+    }
+  }
+}
